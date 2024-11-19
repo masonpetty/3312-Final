8710511c436acdd8f6516adbfc46e057abd9042a
masonpetty <143662675+masonpetty@users.noreply.github.com>
2024-11-19 15:03:43 -0600

    Attendees.cshtml.cs

---
+++ Pages/Attendees.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EventManagementPlatform.Models;
+
+namespace EventManagementPlatform.Pages
+{
+    public class AttendeesModel : PageModel
+    {
+        private readonly EventManagementContext _context;
+
+        public AttendeesModel(EventManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Attendee> Attendees { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Attendees = await _context.Attendees
+                .Include(a => a.EventAttendees)
+                .ThenInclude(ea => ea.Event)
+                .ToListAsync();
+        }
+    }
+}
