a08928bcd022e697ab750aba58d02fc46796bb0c
masonpetty <143662675+masonpetty@users.noreply.github.com>
2024-11-19 14:53:31 -0600

    Update EventManagementContext.cs

--- models/EventManagementContext.cs
+++ models/EventManagementContext.cs
@@ -9,7 +9,24 @@ namespace EventManagementPlatform.Models
         }
 
         public DbSet<Event> Events { get; set; }
-        public DbSet<EventManager> Eventmanagers  { get; set; }
+        public DbSet<EventManager> Eventmanagers { get; set; }
         public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<EventAttendee> EventAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EventAttendee>()
+                .HasKey(ea => new { ea.EventId, ea.AttendeeId });
+
+            modelBuilder.Entity<EventAttendee>()
+                .HasOne(ea => ea.Event)
+                .WithMany(e => e.EventAttendees)
+                .HasForeignKey(ea => ea.EventId);
+
+            modelBuilder.Entity<EventAttendee>()
+                .HasOne(ea => ea.Attendee)
+                .WithMany(a => a.EventAttendees)
+                .HasForeignKey(ea => ea.AttendeeId);
+        }
     }
 }
